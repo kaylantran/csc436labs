672ed7121f8bc58f4cffb75d152402ad22e9b1f4
Kayla <kaylatran2021@gmail.com>
2025-02-20 04:49:36 -0800

    feat: RadioGroup and hoisting NumberField's state

--- PizzaParty/app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
+++ PizzaParty/app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -20,11 +23,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.zybooks.pizzaparty.ui.theme.PizzaPartyTheme
+import kotlin.math.ceil
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,9 +47,24 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+fun calculateNumPizzas(
+    numPeople: Int,
+    hungerLevel: String
+): Int {
+    val slicesPerPizza = 8
+    val slicesPerPerson = when (hungerLevel) {
+        "Light" -> 2
+        "Medium" -> 3
+        else -> 4
+    }
+
+    return ceil(numPeople * slicesPerPerson / slicesPerPizza.toDouble()).toInt()
+}
+
 @Composable
 fun PizzaPartyScreen(modifier: Modifier = Modifier) {
     var totalPizzas by remember { mutableIntStateOf(0) }
+    var numPeopleInput by remember { mutableStateOf("") }
 
     Column(
         modifier = modifier.padding(10.dp)
@@ -56,6 +76,8 @@ fun PizzaPartyScreen(modifier: Modifier = Modifier) {
         )
         NumberField(
             labelText = "Number of people?",
+            textInput = numPeopleInput,
+            onValueChange = { numPeopleInput = it},
             modifier = modifier.padding(bottom = 16.dp).fillMaxWidth()
         )
         RadioGroup(
@@ -83,13 +105,13 @@ fun PizzaPartyScreen(modifier: Modifier = Modifier) {
 @Composable
 fun NumberField(
     labelText: String,
+    textInput: String,
+    onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    var textInput by remember { mutableStateOf("") }
-
     TextField(
         value = textInput,
-        onValueChange = { textInput = it },
+        onValueChange = onValueChange,
         label = { Text(labelText) },
         singleLine = true,
         keyboardOptions = KeyboardOptions(
@@ -104,9 +126,35 @@ fun RadioGroup(
     labelText: String,
     radioOptions: List<String>,
     selectedValue: String,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
-    // Not implemented yet
+    var selectedOption by remember { mutableStateOf(selectedValue) }
+    val isSelectedOption: (String) -> Boolean = { selectedOption == it }
+
+    Column {
+        Text(labelText)
+        radioOptions.forEach { option ->
+            Row(
+                modifier = modifier
+                    .selectable(
+                        selected = isSelectedOption(option),
+                        onClick = { selectedOption = option },
+                        role = Role.RadioButton
+                    )
+                    .padding(8.dp)
+            ) {
+                RadioButton(
+                    selected = isSelectedOption(option),
+                    onClick = null,
+                    modifier = modifier.padding(end = 8.dp)
+                )
+                Text(
+                    text = option,
+                    modifier = modifier.fillMaxWidth()
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
