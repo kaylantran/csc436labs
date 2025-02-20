86c0ff82228e04a938dc4a971b8bd6fd745f0abe
Kayla <kaylatran2021@gmail.com>
2025-02-20 04:40:22 -0800

    feat: NumberField composable

--- PizzaParty/app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
+++ PizzaParty/app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
@@ -7,16 +7,20 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -81,7 +85,18 @@ fun NumberField(
     labelText: String,
     modifier: Modifier = Modifier
 ) {
-    // Not implemented yet
+    var textInput by remember { mutableStateOf("") }
+
+    TextField(
+        value = textInput,
+        onValueChange = { textInput = it },
+        label = { Text(labelText) },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Number
+        ),
+        modifier = modifier
+    )
 }
 
 @Composable
